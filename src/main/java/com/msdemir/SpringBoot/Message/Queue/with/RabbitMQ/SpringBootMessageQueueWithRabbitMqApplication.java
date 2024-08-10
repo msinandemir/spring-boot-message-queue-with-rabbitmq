d2c8a180d9dea3be889506a008272e8dac6ed14e
Sinan <sinandeemir@gmail.com>
2024-08-10 19:14:43 +0300

    init commit

---
+++ src/main/java/com/msdemir/SpringBoot/Message/Queue/with/RabbitMQ/SpringBootMessageQueueWithRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.msdemir.SpringBoot.Message.Queue.with.RabbitMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessageQueueWithRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMessageQueueWithRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msdemir/SpringBoot/Message/Queue/with/RabbitMQ/SpringBootMessageQueueWithRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msdemir.SpringBoot.Message.Queue.with.RabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMessageQueueWithRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
